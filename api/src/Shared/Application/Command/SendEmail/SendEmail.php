5960d26a97cb2e3959c6001891752012b88c5b09
romen2232 <romenmedbel@gmail.com>
2026-09-13 15:45:03 +0200

    feat: add SendEmail message and handler (TDD)

---
+++ api/src/Shared/Application/Command/SendEmail/SendEmail.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Application\Command\SendEmail;
+
+final readonly class SendEmail
+{
+    public function __construct(
+        public string $to,
+        public string $subject,
+        public string $body,
+    ) {
+    }
+}
