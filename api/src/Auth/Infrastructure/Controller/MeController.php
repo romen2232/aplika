2c3a89d54c404f97d7fe9bff32c009b5bfdc0798
romen2232 <romenmedbel@gmail.com>
2026-09-14 14:10:48 +0200

    fix(auth): update route annotation import to use attributes

--- api/src/Auth/Infrastructure/Controller/MeController.php
+++ api/src/Auth/Infrastructure/Controller/MeController.php
@@ -7,7 +7,7 @@ namespace App\Auth\Infrastructure\Controller;
 use App\Auth\Infrastructure\Security\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MeController extends AbstractController
