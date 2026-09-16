f34c18a4c7897455f81b6afebd4f09f5c1267305
victrespy <victrespy@gmail.com>
2026-09-16 12:55:22 +0200

    style: pre-push lint changes

--- frontend/components/common/atoms/Badge.tsx
+++ frontend/components/common/atoms/Badge.tsx
@@ -9,6 +9,6 @@ interface BadgeProps {
 }
 
 export function Badge({ text, bgColor, textColor, borderColor, className = '' }: BadgeProps) {
-  const badgeClasses = `bg-${bgColor} text-${textColor} border-${borderColor} ${className} px-2 py-1 rounded-full text-sm font-medium inline-block border`; 
+  const badgeClasses = `bg-${bgColor} text-${textColor} border-${borderColor} ${className} px-2 py-1 rounded-full text-sm font-medium inline-block border`;
   return <span className={badgeClasses}>{text}</span>;
 }
