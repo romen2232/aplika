d888574e7f53bfe54dcc99fab5643531229b35fc
romen2232 <romenmedbel@gmail.com>
2026-09-16 11:56:53 +0200

    build(deps): add nelmio api-doc bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
